83d616f21be2f82bac68be6cca7d95f7ab998335
PRIMEMAC001 <primemac001@PRIMEMAC001.local>
2026-05-21 16:02:49 -0300

    rodando testes do produtorSevices: não concluido

--- src/main/java/com/kata/produto/service/ProdutoServiceImpl.java
+++ src/main/java/com/kata/produto/service/ProdutoServiceImpl.java
@@ -13,20 +13,66 @@ import java.util.List;
 public class ProdutoServiceImpl implements ProdutoService {
 
 
-    @Override
-    public ProdutoResponse criar(ProdutoRequest request) {
-        return null;
+    private final ProdutoRepository produtoRepository;
+
+    ProdutoServiceImpl(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
     }
 
     @Override
+    public ProdutoResponse criar(ProdutoRequest request) {
+    Produto produto = new Produto();
+    produto.setNome(request.nome());
+    produto.setPreco(request.preco());
+    produto.setDescricao(request.descricao());
+    produto.setQuantidadeEstoque(request.quantidadeEstoque());
+    produto.setCategoria(request.categoria());
+
+    
+    Produto salvo = produtoRepository.save(produto);
+
+    return new ProdutoResponse(
+        salvo.getId(),
+        salvo.getNome(),
+        salvo.getPreco(),
+        salvo.getDescricao(),
+        salvo.getQuantidadeEstoque(),
+        salvo.getCategoria()
+    );
+}
+
+    @Override
     public List<ProdutoResponse> listar() {
-        return List.of();
+    List<Produto> produtos = produtoRepository.findAll();
+        
+        return produtos.stream().map(produto -> new ProdutoResponse(
+        produto.getId(),
+        produto.getNome(),
+        produto.getPreco(),
+        produto.getDescricao(),
+        produto.getQuantidadeEstoque(),
+        produto.getCategoria()
+    ))
+    .toList();
+        
     }
 
     @Override
     public ProdutoResponse buscarPorId(Long id) {
-        return null;
-    }
+    Produto produto = produtoRepository.findById(id)
+    .orElseThrow(() -> new ProdutoNaoEncontradoException(id));
+
+    return new ProdutoResponse(
+        produto.getId(),
+        produto.getNome(),
+        produto.getPreco(),
+        produto.getDescricao(),
+        produto.getQuantidadeEstoque(),
+        produto.getCategoria()
+    );
+}
+
+
 
     @Override
     public ProdutoResponse atualizar(Long id, ProdutoRequest request) {
